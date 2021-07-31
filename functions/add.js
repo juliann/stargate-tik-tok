bba2bf0749fab3d0a3d61cce177331d33ec5a76b
Julian <julian.nadarzy@gmail.com>
2021-07-31 12:05:03 +0200

    added upload feature

---
+++ functions/add.js
@@ -0,0 +1,30 @@
+const faker = require("faker");
+const { createClient } = require("@astrajs/collections");
+
+const collection = "tktkposts2";
+let id = faker.random.uuid();
+
+exports.handler = async function (event, context, callback) {
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+  });
+  const users = astraClient
+    .namespace(process.env.ASTRA_DB_KEYSPACE)
+    .collection(collection);
+
+  try {
+    const user = await users.create(id, event.body);
+
+    return {
+      statusCode: 200,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
+};
